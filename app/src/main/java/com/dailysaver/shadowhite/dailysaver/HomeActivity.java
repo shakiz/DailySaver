e2b2bdfb1cc937f5e145e7c44ed36557fc756cee
shakiz <activities335@gmail.com>
2019-11-05 23:29:26 +0600

    home ui updated 2.0

--- app/src/main/java/com/dailysaver/shadowhite/dailysaver/HomeActivity.java
+++ app/src/main/java/com/dailysaver/shadowhite/dailysaver/HomeActivity.java
@@ -1,9 +1,31 @@
 package com.dailysaver.shadowhite.dailysaver;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.skydoves.powermenu.CircularEffect;
+import com.skydoves.powermenu.CustomPowerMenu;
+import com.skydoves.powermenu.MenuAnimation;
+import com.skydoves.powermenu.MenuBaseAdapter;
+import com.skydoves.powermenu.OnMenuItemClickListener;
+import com.skydoves.powermenu.PowerMenu;
+import com.skydoves.powermenu.PowerMenuItem;
+
 import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
@@ -12,6 +34,9 @@ public class HomeActivity extends AppCompatActivity {
     private CardAdapter cardAdapter;
     private RecyclerView recyclerView;
     private RecyclerView.LayoutManager layoutManager;
+    private RelativeLayout mainLayout;
+    private FloatingActionButton addButton;
+    private CustomPowerMenu powerMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +50,24 @@ public class HomeActivity extends AppCompatActivity {
 
     private void bindUiWithComponents() {
         setAdapter();
+
+        powerMenu =new CustomPowerMenu.Builder<>(this, new IconMenuAdapter())
+                .addItem(new IconPowerMenuItem(ContextCompat.getDrawable(this, R.drawable.ic_expense), getResources().getString(R.string.add_new_expense)))
+                .addItem(new IconPowerMenuItem(ContextCompat.getDrawable(this, R.drawable.ic_wallet), getResources().getString(R.string.add_new_wallet)))
+                .setAnimation(MenuAnimation.SHOWUP_TOP_LEFT) // Animation start point (TOP | LEFT).
+                .setMenuRadius(15f) // sets the corner radius.
+                .setMenuShadow(10f) // sets the shadow.
+                .setCircularEffect(CircularEffect.INNER)
+                .build();
+
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                powerMenu.showAsAnchorCenter(mainLayout);
+            }
+        });
+
     }
 
     private void setAdapter() {
@@ -45,7 +88,29 @@ public class HomeActivity extends AppCompatActivity {
 
     private void init() {
         recyclerView = findViewById(R.id.dashboardRecyclerView);
+        mainLayout = findViewById(R.id.home_layout);
+        addButton = findViewById(R.id.add);
         cardItemList = new ArrayList<>();
+    }
+
+    public class IconMenuAdapter extends MenuBaseAdapter<IconPowerMenuItem> {
+
+        @Override
+        public View getView(int index, View view, ViewGroup viewGroup) {
+            final Context context = viewGroup.getContext();
+
+            if(view == null) {
+                LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                view = inflater.inflate(R.layout.item_icon_menu, viewGroup, false);
+            }
+
+            IconPowerMenuItem item = (IconPowerMenuItem) getItem(index);
+            final ImageView icon = view.findViewById(R.id.item_icon);
+            icon.setImageDrawable(item.getIcon());
+            final TextView title = view.findViewById(R.id.item_title);
+            title.setText(item.getTitle());
+            return super.getView(index, view, viewGroup);
+        }
     }
 }
 
